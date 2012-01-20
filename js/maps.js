a80f1488215506c589b44e8f94d33e06444554e3
Matthew Phillips <matthew@phillipsoft.biz>
2012-01-19 22:42:19 -0500

    Modified the logic a bit.

--- js/maps.js
+++ js/maps.js
@@ -4,22 +4,27 @@
 (function() {
 'use strict';
 
+function createTileLayer(maxZoom) {
+	maxZoom = maxZoom || 18;
+
+	const TITLE_URL = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png';
+	var subdomains = [ 'otile1', 'otile2', 'otile3', 'otile4' ],
+		attribution = 'Temporary attribution.',
+		tileLayer = new L.TileLayer(TITLE_URL, {
+			maxZoom: maxZoom,
+			attribution: attribution,
+			subdomains: subdomains
+		});
+}
+
 var Map = {
 	init: function() {
-		var tileUrl = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
-      subdomains = [ 'otile1', 'otile2', 'otile3', 'otile4' ],
-      attribution = 'Temporary attribution.',
-      tileLayer = new L.TileLayer(tileUrl, {
-        maxZoom: 18,
-        attribution: attribution,
-        subdomains: subdomains
-      });
+		var tileLayer = createTileLayer();
+		var map = new L.Map('map');
+		map.setView(new L.LatLng(51.505, -0.09), 13).addLayer(tileLayer);
 
-      var map = new L.Map('map');
-      map.setView(new L.LatLng(51.505, -0.09), 13).addLayer(tileLayer);
-
-      var marker = new L.Marker(new L.LatLng(51.5, -0.09));
-      map.addLayer(marker);
+		var marker = new L.Marker(new L.LatLng(51.5, -0.09));
+		map.addLayer(marker);
 	},
 
   handleEvent: function(e) {

dd2f3d666693dd3b315f5cb8928928c8707a6727
Matthew Phillips <matthew@phillipsoft.biz>
2012-01-19 21:32:42 -0500

    Made changes to use Leaflet maps.

--- js/maps.js
+++ js/maps.js
@@ -6,17 +6,20 @@
 
 var Map = {
 	init: function() {
-		var elem = document.getElementById('map');
-    elem.style.width = window.innerWidth + 'px';
-    elem.style.height = window.innerHeight + 'px';
-    ['tap', 'touchstart', 'touchend', 'touchmove'].forEach(function(evt) {
-      elem.addEventListener(evt, Map);
-    });
+		var tileUrl = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
+      subdomains = [ 'otile1', 'otile2', 'otile3', 'otile4' ],
+      attribution = 'Temporary attribution.',
+      tileLayer = new L.TileLayer(tileUrl, {
+        maxZoom: 18,
+        attribution: attribution,
+        subdomains: subdomains
+      });
 
-		window.map = new MQA.TileMap(elem, 9, {
-			lat: 39.743943,
-			lng: -105.020089
-		}, 'map');
+      var map = new L.Map('map');
+      map.setView(new L.LatLng(51.505, -0.09), 13).addLayer(tileLayer);
+
+      var marker = new L.Marker(new L.LatLng(51.5, -0.09));
+      map.addLayer(marker);
 	},
 
   handleEvent: function(e) {

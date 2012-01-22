2d66692f5f2da78d9ba3846ba4fbcc91f15f8759
Matthew Phillips <matthew@phillipsoft.biz>
2012-01-21 19:37:29 -0500

    Bug fixes

--- js/maps.js
+++ js/maps.js
@@ -7,30 +7,29 @@
 var Map;
 
 function createTileLayer(maxZoom) {
-	maxZoom = maxZoom || 18;
+  maxZoom = maxZoom || 18;
 
-	const TITLE_URL = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png';
-	var subdomains = [ 'otile1', 'otile2', 'otile3', 'otile4' ],
-		attribution = 'Temporary attribution.';
-	return new L.TileLayer(TITLE_URL, {
-		maxZoom: maxZoom,
-		attribution: attribution,
-		subdomains: subdomains
-	});
+  var TITLE_URL = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png';
+  var subdomains = [ 'otile1', 'otile2', 'otile3', 'otile4' ],
+    attribution = 'Temporary attribution.';
+  return new L.TileLayer(TITLE_URL, {
+    maxZoom: maxZoom,
+    attribution: attribution,
+    subdomains: subdomains
+  });
 }
 
 Map = {
-	init: function() {
-		var tileLayer = createTileLayer();
-		var map = new L.Map('map');
-		map.setView(new L.LatLng(51.505, -0.09), 13).addLayer(tileLayer);
+  init: function() {
+    var tileLayer = createTileLayer();
+    var map = new L.Map('map');
+    map.setView(new L.LatLng(51.505, -0.09), 13).addLayer(tileLayer);
 
-		var marker = new L.Marker(new L.LatLng(51.5, -0.09));
-		map.addLayer(marker);
-	},
+    var marker = new L.Marker(new L.LatLng(51.5, -0.09));
+    map.addLayer(marker);
 
-  handleEvent: function(e) {
-    e.preventDefault();
+    this.nav = new Nav;
+    this.nav.init();
   }
 };
 
@@ -39,9 +38,12 @@ function Nav() {
 }
 
 Nav.prototype = {
-  init: {
-    var current = new NavBtn(document.getElementById('current'));
+  init: function() {
+    var current = new NavBtn(document.getElementById('current'), getCurrent);
     var search = new NavBtn(document.getElementById('search'));
+
+    current.listen();
+    search.listen();
   },
 
   getCurrent: function() {
@@ -59,20 +61,20 @@ NavBtn.prototype = {
     var self = this;
 
     var evts = [ 'touchstart', 'touchmove', 'touchend',
-      'mousestart', 'mousemove', 'mouseend' ];
+      'mousedown', 'mousemove', 'mouseup' ];
     evts.forEach(function(evt) {
-      self.node.addEventListener(evt, self.bind(self), true);
+      self.node.addEventListener(evt, self, true);
     });
   },
 
   handleEvent: function(e) {
     switch(e.type) {
       case 'touchend':
-      case 'mouseend':
+      case 'mouseup':
         this.onclick();
         break;
       case 'touchstart':
-      case 'mousestart':
+      case 'mousedown':
         // TODO highlight node.
         break;
       case 'touchmove':
@@ -80,12 +82,14 @@ NavBtn.prototype = {
         // TODO see if we've left the bounds.
         break;
     }
-  }
+  },
+
+  onclick: function() { }
 };
 
 window.addEventListener('load', function loadMap(e) {
-	window.removeEventListener('load', loadMap);
-	Map.init();
+  window.removeEventListener('load', loadMap);
+  Map.init();
 });
 
 
